27e4e93bfe9d16f3485e286a6036c91f405b8968
Mbali <mbalikhumalo408@gmail.com>
2023-07-31 13:56:11 +0200

    drag

--- App.js
+++ App.js
@@ -1,11 +1,33 @@
 import * as React from "react";
-import MapView from "react-native-maps";
+import MapView, { Callout, Circle, Marker } from "react-native-maps";
 import { StyleSheet, Text, View, Dimensions } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <MapView style={styles.map}/>
+      <MapView style={styles.map}
+        initialRegion={{
+          latitude: -26.12226,
+          longitude: 28.05355,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}>
+        <Marker coordinate={{
+          latitude: -26.12226,
+          longitude: 28.05355
+        }}
+          pinColor="gold"
+        >
+          <Callout>
+            <Text>This is where you are</Text>
+          </Callout>
+        </Marker>
+
+        <Circle center={{
+          latitude: -26.12226,
+          longitude: 28.05355,
+        }} radius={100} />
+      </MapView>
     </View>
   );
 }
@@ -16,7 +38,7 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
-  }, 
+  },
   map: {
     width: Dimensions.get("window").width,
     height: Dimensions.get("window").height
